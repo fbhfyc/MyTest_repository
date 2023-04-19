b1870ba8b678b386f431796ba9e62bf8ca4cee13
x x <fbhfyc@163.com>
2023-04-19 10:36:10 +0800

    Initial commit

---
+++ Project1/源.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+//#include <algorithm>
+
+using namespace std;
+
+int main() {
+    vector<int> vec = { 1, 2, 3, 4, 5 };
+
+    // 在vector中查找元素2
+    auto it = find(vec.begin(), vec.end(), 2);
+
+    // 判断是否找到
+    if (it != vec.end()) {
+        cout << "找到了，位置为：" << it - vec.begin() << endl;
+    }
+    else {
+        cout << "没找到！" << endl;
+    }
+
+    return 0;
+}
