8e8faaaaea7c4077f75224076ee3c65c06fa20f1
x x <fbhfyc@163.com>
2023-05-05 13:14:41 +0800

    fdsj

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,82 +1,71 @@
 #include "base.h"
 #include "test_all.h"
-////
-//  vector test
-// vec_test_find();
-//vec_test_popback();
- /*   string str{"abcdefg"};
-    string::iterator it;*/
-    //str.append(" fbh");
-    //str += " & fyc";
-    //str.push_back('41');
-   // str.insert( 3," insert");
-    //str.erase(1,1);
-   // int pos = str.rfind("b");
-    //string temp = str.substr(2);
-    //str.replace(1,4,"hhhh");
-    //cout << str<<endl;
-  /*  for ( it=str.begin();it != str.end();++it)
-    cout   << *it ;*/
+ 
+#include "spdlog/spdlog.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/rotating_file_sink.h"// support for rotating file logging
+#include "spdlog/sinks/basic_file_sink.h"// support for basic file logging
+#include "spdlog/sinks/daily_file_sink.h"
+#include "spdlog/async.h" //support for async logging.
+using namespace spdlog;
 
-extern CMyVector vv;
-
-class Student {
-private:
-    int m_id;
-    string m_name;
-public:
-    Student(const string& name, int id):
-        m_id(id),m_name(name)
-    {}
-    int getId() const;
-    void introduce() const;
-};
-int Student::getId() const {
-    return m_id;
-}
-void Student::introduce() const {
-    cout << m_id<< ":" <<m_name << endl;
+void stdout_example()
+{
+	// create color multi threaded logger
+	auto console = spdlog::stdout_color_st("console");
+	auto err_logger = spdlog::stderr_color_mt("stderr");
+	spdlog::get("console")->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name)");
 }
 
-typedef bool(*func_t)(const Student&,const Student&);
-bool stuCmp(const Student& s1,const Student& s2) {
-    return s1.getId() < s2.getId();
+auto rotating_logger = rotating_logger_mt("mylog", "logs/rotating.txt", 1048576 * 5, 3);
+
+int main(int, char* [])
+{
+	/*
+	int a = 10000;
+	//<1.创建名称为LoggerName1、内容输出到控制台的单线程版本日志记录器
+	  spdlog::stdout_color_st("LoggerName1");
+	spdlog::get("LoggerName1")->info("test spdlog::get(logger_name)");
+	//<2.创建名称为LoggerName2、内容输出到Logs/BasicFileLog.txt的
+	//多线程版本日志记录器
+	auto logger2 = spdlog::basic_logger_mt("LoggerName2", "Logs/BasicFileLog.txt");
+	spdlog::get("LoggerName2")->info("a={}",a);
+	//<3.创建名称为LoggerName3、内容输出到Logs/RotatingFileLog.txt的
+	// //多线程版本日志记录器
+	//参数1024*1024*5设置了文件最大容量为5mb，参数3设置了文件最大数量为3
+	//当日志文件存储超过5mb时，将重命名日志文件并且新增另外一个日志文件
+	//当日志文件数量超过3时，将删除第一个日志文件
+
+	auto logger3 = spdlog::rotating_logger_mt("LoggerName3", "Logs/RotatingFileLog.txt", 1024 * 1024 * 5, 3);
+
+	//<4.创建名称为LoggerName4、
+	// 内容输出到Logs/DailyFileLog.txt的多线程版本日志记录器
+	//参数2和30指定每天生成日志文件的时间为凌晨2点30分
+
+	auto logger4 = spdlog::daily_logger_mt("LoggerName4", "Logs/DailyFileLog.txt", 2, 30);
+
+	system("pause");
+	*/
+
+	try
+	{
+		auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+		console_sink->set_level(spdlog::level::warn);
+		console_sink->set_pattern("[multi_sink_example] [%^%l%$] %v");
+
+		auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/multisink.txt", true);
+		file_sink->set_level(spdlog::level::trace);
+
+		spdlog::logger logger("multi_sink", { console_sink, file_sink });
+		logger.set_level(spdlog::level::debug);
+		logger.warn("this should appear in both console and file");
+		logger.info("this message should not appear in the console, only in the file");
+	}
+	catch (const spdlog::spdlog_ex& ex)
+	{
+		std::cout << "Log initialization failed: " << ex.what() << std::endl;
+	}
+ 
+	
+	return 1;
 }
-
-struct MyString {
-    string m_str;
-    MyString(string str):m_str(str) {
-        cout << "[Construct:] " << m_str <<endl;
-    }
-    ~MyString() {
-        cout << "[Deconstruct:] " << m_str << endl;
-    }
-    string getStr() {
-        return m_str;
-    }
-};
-
-class CA {
-public:
-    int fun1() {
-        return m_i;
-    }
-    int m_i;
-};
-
-int main() {
-   
-    stack<int> ss;
-    queue<int> q;
-    priority_queue<int> pq;
-    set<int> s;
-    
-    CA ca;
-    cout << sizeof(ca) << endl;
-
-    unique_ptr<MyString> p1 = std::make_unique<MyString>("first string");
-    //unique_ptr<MyString> p2 = std::make_unique<MyString>("jkjkj");
-    p1.reset(new MyString("reset new"));
-    cout << p1->getStr();
-    return 0; 
-} 
